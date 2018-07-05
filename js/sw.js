9ef51d9aba84096045f1a435ad00d14d2bc63e6c
tcly861204 <tcly861204@hotmail.com>
2018-07-05 09:35:18 +0800

    pwa实现

---
+++ js/sw.js
@@ -0,0 +1,41 @@
+const cacheName="pwa-step-4";
+const filesToCache = [
+  "/pwa/js/basic.js",
+  "/pwa/css/main.css",
+  "/pwa/img/face.jpg",
+  "/pwa/index.html",
+  "/pwa/",
+];
+
+function updateStaticCache(){
+  return caches.open(cacheName)
+  .then(function(cache){
+    return cache.addAll(filesToCache);
+  })
+  .then(()=>self.skipWaiting());
+}
+
+self.addEventListener('install',function(event){
+  event.waitUntil(updateStaticCache());
+},false);
+
+self.addEventListener('activate',function(event){
+  event.waitUntil(caches.keys().then(function(keysList){
+    return Promise.all(keysList.map(function(key){
+        if(key !== cacheName){
+            return caches.delete(key);
+        }
+    }));
+  }));
+},false);
+
+self.addEventListener('fetch',function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(response){
+      if(response!==null){
+        return response;
+      }
+      return fetch(event.request.url);
+    })
+  )
+},false);
